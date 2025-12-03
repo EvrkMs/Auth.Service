8eb4cf09bcc21d345b3cb6d67f2273f42c634e66
EvrkMs <evrk.msn@gmail.com>
2025-12-03 20:42:49 +0300

    entity type

---
+++ src/Engine/Auth.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Globalization;
+
+namespace Auth.Domain.Entities;
+
+public class User : IdentityUser<Guid>
+{
+    public string DispayName { get; set; } = null!;
+}
